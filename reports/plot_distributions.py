f3738e8882127f38e8e7150161f6f120000b7db9
Joakim Edin <Joakim Edin>
2024-07-11 15:38:00 +0000

    update limits comprarison plot

--- reports/plot_distributions.py
+++ reports/plot_distributions.py
@@ -4,8 +4,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
+dataset_names = ["yelp", "sst2"]
 model_names = [
-    # "JiaqiLee/robust-bert-yelp",
+    "textattack/bert-base-uncased-SST-2",
+    "textattack/roberta-base-SST-2",
     "textattack/bert-base-uncased-yelp-polarity",
     "VictorSanh/roberta-base-finetuned-yelp-polarity",
     "textattack/bert-base-uncased-imdb",
@@ -13,46 +15,47 @@ model_names = [
 ]
 
 model_dict = {
-    "JiaqiLee/robust-bert-yelp": "Robust BERT Yelp",
-    "textattack/bert-base-uncased-yelp-polarity": "BERT Yelp",
-    "VictorSanh/roberta-base-finetuned-yelp-polarity": "RoBERTa Yelp",
-    "textattack/bert-base-uncased-imdb": "BERT IMDB",
-    "textattack/roberta-base-imdb": "RoBERTa IMDB",
+    "textattack/roberta-base-SST-2": "RoBERTa$_{\\text{SST2}}$",
+    "textattack/bert-base-uncased-SST-2": "BERT$_{\\text{SST2}}$",
+    "textattack/bert-base-uncased-yelp-polarity": "BERT$_{\\text{Yelp}}$",
+    "VictorSanh/roberta-base-finetuned-yelp-polarity": "RoBERTa$_{\\text{Yelp}}$",
+    "textattack/bert-base-uncased-imdb": "BERT$_{\\text{IMDB}}$",
+    "textattack/roberta-base-imdb": "RoBERTa$_{\\text{IMDB}}$",
 }
-
-dataframes = []
-for model_name in model_names:
-    file_path = Path(f"results/yelp_best_scores_{model_name.split('/')[1]}.parquet")
-    if not file_path.exists():
-        continue
-    temp_df = pd.read_parquet(file_path)
-    temp_df["model"] = model_dict[model_name]
-    dataframes.append(temp_df)
-
-
-df = pd.concat(dataframes)
-sns.set_theme(style="whitegrid", context="paper", palette="colorblind", font_scale=1.5)
-g = sns.histplot(data=df, x="comprehensiveness", hue="model", kde=True, bins=20)
-plt.xlabel("AOPC score")
-plt.ylabel("Frequency")
-# no legend title for the model
-plt.legend(title=None)
-# plot vertical line representing the average
-print(df.groupby("model")["comprehensiveness"].describe())
-print(df.groupby("model")["sufficiency"].describe())
-plt.tight_layout()
-plt.xlim(-0.1, 1.1)
-
-plt.savefig("results/comprehensiveness.pdf", format="pdf")
-
-plt.clf()
+for dataset_name in dataset_names:
+    dataframes = []
+    for model_name in model_names:
+        file_path = Path(f"results/aopc_limits_exact/{dataset_name}_{model_name.split('/')[1]}.parquet")
+        if not file_path.exists():
+            continue
+        temp_df = pd.read_parquet(file_path)
+        temp_df["model"] = model_dict[model_name]
+        dataframes.append(temp_df)
 
 
-g=sns.histplot(data=df, x="sufficiency", hue="model", kde=True, bins=50)
-plt.xlabel("AOPC score")
-plt.ylabel("Frequency")
-plt.xlim(-0.2, 0.5)
-plt.tight_layout()
-g.legend_.set_title(None)
+    df = pd.concat(dataframes)
+    sns.set_theme(style="whitegrid", context="paper", palette="colorblind", font_scale=1.5)
+    g = sns.histplot(data=df, x="upper_limit", hue="model", kde=True, bins=20)
+    plt.xlabel("AOPC score")
+    plt.ylabel("Frequency")
+    # no legend title for the model
+    plt.legend(title=None)
+    # plot vertical line representing the average
+    print(df.groupby("model")["upper_limit"].describe())
+    print(df.groupby("model")["lower_limit"].describe())
+    plt.tight_layout()
+    plt.xlim(-0.1, 1.1)
 
-plt.savefig("results/sufficiency.pdf", format="pdf")
+    plt.savefig(f"figures/{dataset_name}_upper_limit.pdf", format="pdf")
+
+    plt.clf()
+
+
+    g=sns.histplot(data=df, x="lower_limit", hue="model", kde=True, bins=50)
+    plt.xlabel("AOPC score")
+    plt.ylabel("Frequency")
+    plt.xlim(-0.2, 0.5)
+    plt.tight_layout()
+    g.legend_.set_title(None)
+
+    plt.savefig(f"figures/{dataset_name}_lower_limit.pdf", format="pdf")
