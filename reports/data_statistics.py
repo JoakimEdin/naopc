f99c852d35eb7507b2836779d7299a21a455315a
JoakimEdin <joakim_edin@hotmail.com>
2024-12-11 14:09:13 +0100

    add data statistics for new datasets

--- reports/data_statistics.py
+++ reports/data_statistics.py
@@ -7,18 +7,20 @@ from matplotlib import pyplot as plt
 imdb = load_dataset("csv", data_files="data/imdb_test_long.csv", split="train")
 sst2 = load_dataset("csv", data_files="data/sst2_test_long.csv", split="train")
 yelp = load_dataset("csv", data_files="data/yelp_test_long.csv", split="train")
+snli = load_dataset("csv", data_files="data/snli_test_long.csv", split="train")
+agnews = load_dataset("csv", data_files="data/ag_news_test_long.csv", split="train")
 
-for i, (dataset, name) in enumerate(zip([sst2, yelp, imdb], ["SST-2", "Yelp", "IMDB"])):
+snli = snli.map(lambda x: {"text": x["premise"] + " " + x["hypothesis"]})
+
+for i, (dataset, name) in enumerate(zip([sst2, yelp, imdb, snli, agnews], ["SST-2", "Yelp", "IMDB", "SNLI", "AG News"])):
     lengths = dataset.with_format("pandas")["text"].str.split().apply(len)
     print(name, lengths.describe())
 
 
-
-
 # plot length histograms
 
-fig, axs = plt.subplots(3, 1, figsize=(10, 15))
-for i, (dataset, name) in enumerate(zip([sst2, imdb, yelp], ["SST-2", "IMDB", "Yelp"])):
+fig, axs = plt.subplots(5, 1, figsize=(10, 25))
+for i, (dataset, name) in enumerate(zip([sst2, yelp, imdb, snli, agnews], ["SST-2", "Yelp", "IMDB", "SNLI", "AG News"])):
     lengths = dataset.with_format("pandas")["text"].str.split().apply(len)
     axs[i].hist(lengths, bins=50)
     axs[i].set_title(f"{name} length distribution")
